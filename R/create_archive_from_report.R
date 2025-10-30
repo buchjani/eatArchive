e184df8776f7822ba46acdc0080ebea1f03b7fc9
buchjani <janine.buchholz@hu-berlin.de>
2025-10-30 17:11:36 +0100

    modify columns in report #16

--- R/create_archive_from_report.R
+++ R/create_archive_from_report.R
@@ -1,6 +1,6 @@
 #' Create an archival directory based on Excel file
 #'
-#' @param path_to_directory_report Character. Path to Excel file resulting from `write_directory_report()` containing column "AIP", indicating whether and where to archive the file.
+#' @param path_to_directory_report Character. Path to Excel file resulting from `write_directory_report()` containing column "Archive", indicating whether and where to archive the file.
 #' @param path_to_archive_directory Character. Path to ....
 #' @param convert Logical, indicating whether to convert file formats or not ....
 #' @param overwrite Logical, indicating whether to overwrite existing files ....
@@ -22,15 +22,15 @@ create_archive_from_report <- function(path_to_directory_report,
   # combine sheets in dataframe
   df <- .combine_excel_sheets(path_to_directory_report)
 
-  # check if column AIP exists - if not, return message that function doesnt kknow what to move
-  if("AIP" %in% colnames(df) == FALSE){
-    stop("Column 'AIP' is missing. Please use function `write_directory_report()` and specify argument `autocomplete_values`.")
+  # check if column Archive exists - if not, return message that function doesnt kknow what to move
+  if("Archive" %in% colnames(df) == FALSE){
+    stop("Column 'Archive' is missing. Please use function `write_directory_report()` and specify argument `autocomplete_values`.")
   }
 
   # keep only rows of files to be archived
-  df <- df[!is.na(df$AIP) & !is.na(df$File_Name),]
+  df <- df[!is.na(df$Archive) & !is.na(df$File_Name),]
   if(nrow(df) == 0){
-    stop("Column 'AIP' is empty across all sheets of your Excel file. Looks like there's nothing to be archived.") # ¯\\_(ツ)_/¯
+    stop("Column 'Archive' is empty across all sheets of your Excel file. Looks like there's nothing to be archived.") # ¯\\_(ツ)_/¯
   }
 
   # create archival folder and subfolders as indicated
@@ -39,31 +39,37 @@ create_archive_from_report <- function(path_to_directory_report,
     message(paste("Info. New folder has been created:", path_to_archive_directory))
   }
 
-  subdirs <- paste0(path_to_archive_directory, "/", unique(df$AIP))
+  subdirs <- paste0(path_to_archive_directory, "/", unique(df$Archive))
   invisible(lapply(subdirs, dir.create, recursive = TRUE, showWarnings = FALSE))
 
-  # move files to folder indicated in column "AIP"
-  cat(paste(" Creating AIP...\n",
+  # MOVING FILES ####
+
+  # move files to folder indicated in column "Archive"
+  cat(paste(" Creating Archive...\n",
             "- Copying from: ", .longest_common_path(df$File_Name), "\n",
             "- Copying to:   ", path_to_archive_directory, "\n",
             "- Files to copy:", nrow(df), "\n",
-            "- Size (KB):    ", sum(df$Size_Bytes/1000), "\n"
+            "- Size (KB):    ", sum(df$Size_Bytes/1000), "\n\n"
             ))
 
   invisible(mapply(file.copy,
                    from = df$File_Name,
-                   to = paste0(path_to_archive_directory, "/", df$AIP),
+                   to = paste0(path_to_archive_directory, "/", df$Archive),
                    copy.date = TRUE,
                    overwrite = overwrite))
 
 
   # write a report of what has been moved to where
   report <- data.frame(File_Name = basename(df$File_Name),
-                       Dir_Origin = df$File_Name,
-                       Dir_Archive =  paste0(path_to_archive_directory, "/", df$AIP, "/", basename(df$File_Name)),
-                       Converted = FALSE)
+                       Last_Modified = df$Last_Modified,
+                       Size_Bytes = df$Size_Bytes,
+                       Converted = FALSE,
+                       Dir_Archive =  paste0(path_to_archive_directory, "/", df$Archive, "/", basename(df$File_Name)),
+                       Dir_Origin = df$File_Name
+                       )
 
-  ## Converting files
+  # CONVERTING FILES ####
+
   # xlsx --> csv
   if(convert == TRUE){
 
@@ -72,13 +78,16 @@ create_archive_from_report <- function(path_to_directory_report,
 
       for (i in 1:nrow(df_xlsx)){
         csv_names <- .convert_xlsx_to_csv(xlsx_path = df_xlsx$File_Name[i],
-                             save_to = paste0(path_to_archive_directory, "/", df_xlsx$AIP[i]),
+                             save_to = paste0(path_to_archive_directory, "/", df_xlsx$Archive[i]),
                              overwrite = overwrite)
         report <- rbind(report,
-                        data.frame(File_Name = rep(basename(df_xlsx$File_Name[i]), times = length(csv_names)),
-                                   Dir_Origin = rep(df_xlsx$File_Name[i], times = length(csv_names)),
-                                   Dir_Archive =  csv_names,
-                                   Converted = rep(TRUE, times = length(csv_names)))
+                        data.frame(
+                          File_Name = basename(csv_names),
+                          Last_Modified = df_xlsx$Last_Modified[i],
+                          Size_Bytes = df_xlsx$Size_Bytes[i],
+                          Converted = TRUE,
+                          Dir_Archive = csv_names,
+                          Dir_Origin = rep(df_xlsx$File_Name[i], times = length(csv_names)))
         )
       }
     }
@@ -91,7 +100,8 @@ create_archive_from_report <- function(path_to_directory_report,
                       path = paste0(path_to_archive_directory, "/_archive_documentation.csv"), sep = ",", overwrite = overwrite)
 
   # print message
-  cat(paste0(" AIP hast been created!\n",
+  cat(paste0("\n",
+             " Archive hast been created!\n",
              " Documentation: ", path_to_archive_directory, "/_archive_documentation.csv"))
 
   # invisibly return report

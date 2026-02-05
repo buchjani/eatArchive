9f022bd2658165766440968d650286e80189636c
buchjani <janine.buchholz@hu-berlin.de>
2026-02-05 15:56:00 +0100

    added new main function

---
+++ R/scan_directory.R
@@ -0,0 +1,118 @@
+#' Search a directory for specific files (date, size)
+#'
+#' Scans each immediate subfolder of the specified directory (excluding any listed in `exclude_folders`)
+#' and writes an Excel file containing file-level metadata for specific files. Files can be selected by
+#' size and by date of last modification.
+#'
+#' @param dir Character. Path to the directory whose immediate subfolders should be scanned.
+#' @param path_to_report Character. Path to the Excel file to be written (e.g., "C:/study/dir_report.xlsx").
+#' @param show_what Character, indicating whehter to sort by size or date.
+#' \itemize{
+#'   \item `biggest` for sorting by file size (large to small)
+#'   \item `smallest` for sorting by file size (small to large)
+#'   \item `newest` for sorting by date of last modification (new to old)
+#'   \item `oldest` for sorting by date of last modification (old to new)
+#' }
+#' @param show_max Numeric, indicating the maximum number of files to include in the report.
+#' @param exclude_folders Character vector. Names of subfolders to exclude from processing.
+#' @param overwrite Logical, indicating whether to overwrite existing report.
+#'
+#' @details
+#' The Excel workbook includes a recursive listing of all files meeting the search criteria (including sub-subfolders).
+#'
+#' Files located in any subfolder whose name matches one of the entries in
+#' \code{exclude_folders} will be excluded entirely (i.e., they are not scanned or listed).
+#'
+#' @return Invisibly returns the path to the written Excel file.
+#'
+#' @export
+
+scan_directory <- function(dir,
+                           path_to_report,
+                           exclude_folders = "",
+                           show_what,
+                           show_max = 1000,
+                           overwrite = FALSE) {
+
+  # Check if report file can be created
+  if(dir.exists(path_to_report) & overwrite == FALSE){
+    stop(paste0("\nThe file you indicated already exists:\n",
+                path_to_report,
+                "\nDelete this file or set 'overwrite = TRUE'"))
+  }
+
+  # Write message in console
+  cat(paste0("Scanning base directory: ", "\n", dir, "\n", "\n"))
+
+  # Create workbook
+  wb <- openxlsx::createWorkbook()
+
+  # Toplevel files (non-recursive)
+  toplevel_df <- .get_file_info(dir = dir, recursive = FALSE)
+  overview_df <- data.frame("Folder" = dir, "Size_Bytes" = sum(toplevel_df$Size_Bytes, na.rm = T))
+
+  # Files in subfolders (one sheet per subfolder)
+  subfolders <- fs::dir_ls(dir, type = "directory", recurse = FALSE)
+  subfolders <- subfolders[!fs::path_file(subfolders) %in% exclude_folders]
+  subfolders_df <- data.frame()
+
+  for (f in 1:length(subfolders)) {
+    folder <- subfolders[f]
+    folder_name <- basename(normalizePath(folder))
+
+    # Print progress
+    cat(paste0("- Folder [", f, "/", length(subfolders), "]: ", basename(folder), "\n"))
+
+    # collect data
+    df <- .get_file_info(dir = folder, recursive = TRUE)
+
+    # Only keep  subdirectories - but without those mentioned in exclude_folders (recursively!)
+    if (!is.null(df) && nrow(df) > 0 && length(exclude_folders) > 0) {
+      parts <- fs::path_split(df$File_Name)
+      keep  <- !vapply(parts, function(x) any(x %in% exclude_folders), logical(1))
+      df    <- df[keep, , drop = FALSE]
+    }
+    subfolders_df <- rbind(subfolders_df, df)
+    overview_df <-  rbind(overview_df,
+                          data.frame("Folder" = folder, "Size_Bytes" = sum(df$Size_Bytes, na.rm = T)))
+    }
+
+  df <- rbind(toplevel_df, subfolders_df)
+
+  # write overview sheet
+  .add_sheet_overview(wb = wb, sheet_name = "SUMMARY", df = overview_df)
+
+  # write one sheet for each search criterion
+  if("largest" %in% tolower(show_what)){
+    df_sorted <- df[order(df$Size_Bytes, decreasing = TRUE),]
+    .add_sheet_for_size(wb = wb, sheet_name = "Size (Largest)", dir = dir, df = df_sorted, show_max = show_max, subtitle = "(Large to small)")
+  }
+  if("smallest" %in% tolower(show_what)){
+    df_sorted <- df[order(df$Size_Bytes, decreasing = FALSE),]
+    .add_sheet_for_size(wb = wb, sheet_name = "Size (Smallest)", dir = dir, df = df_sorted, show_max = show_max, subtitle = "(Small to large)")
+  }
+  if("newest" %in% tolower(show_what)){
+    df_sorted <- df[order(df$Last_Modified, decreasing = TRUE),]
+    .add_sheet_for_date(wb = wb, sheet_name = "Date (Newest)", dir = dir, df = df_sorted, show_max = show_max, subtitle = "(New to old)")
+  }
+  if("oldest" %in% tolower(show_what)){
+    df_sorted <- df[order(df$Last_Modified, decreasing = FALSE),]
+    .add_sheet_for_date(wb = wb, sheet_name = "Date (Oldest)", dir = dir, df = df_sorted, show_max = show_max, subtitle = "(Old to new)")
+  }
+
+  # Save the final workbook, write message
+  cat("\nWriting directory report to:\n")
+  cat(path_to_report)
+  openxlsx::saveWorkbook(wb, path_to_report, overwrite = TRUE)
+  invisible(path_to_report)
+}
+
+# Check:
+# scan_directory(
+#   dir = "Q:/FDZ/Alle/20_Veranstaltungen",
+#   path_to_report = "Q:/FDZ/Alle/20_Veranstaltungen/Ordner-Bericht-Auswahl.xlsx",
+#   # exclude_folders = c("_Archive", "_Archiv"),
+#   show_what = c("Largest", "SMALLEST", "neWest", "OLDEst"),
+#   show_max = 100,
+#   overwrite = TRUE
+# )
--- R/utils_add_sheet_with_style.R
+++ R/utils_add_sheet_with_style.R
@@ -1,4 +1,4 @@
-#' Format the excel sheet
+#' Format an excel sheet
 #'
 #' @param wb An excel workbook.
 #' @param sheet_name Name of the excel sheet.
@@ -81,3 +81,199 @@
                      rows = last_row, cols = 1:ncol(df),
                      gridExpand = TRUE, stack = TRUE)
 }
+
+.add_sheet_overview <- function(wb, sheet_name, df, autocomplete_values = NULL) {
+  openxlsx::addWorksheet(wb, sheet_name)
+
+  # Add the data table to openxlsx object
+  openxlsx::writeData(wb, sheet = sheet_name, x = df, startRow = 1, withFilter = TRUE)
+
+  # Header
+  headerStyle <- openxlsx::createStyle(fontSize = 12, fontColour = "#FFFFFF", halign = "left",
+                                       fgFill = "#B40036", border = "TopBottom", borderColour = "#B40036")
+  openxlsx::addStyle(wb, sheet = sheet_name, cols = 1: ncol(df), rows = 1, style = headerStyle,
+                     gridExpand = TRUE, stack = TRUE)
+  openxlsx::freezePane(wb, sheet = sheet_name, firstRow = TRUE)
+
+  # Format column 'Size_Bytes': comma-separated thousands
+  commastyle <- openxlsx::createStyle(numFmt = "COMMA")
+  openxlsx::addStyle(wb, sheet = sheet_name, style = commastyle,
+                     cols = 2, rows = 2:(nrow(df) + 2), gridExpand = TRUE, stack = TRUE)
+
+  # Set column widths and styles
+  openxlsx::setColWidths(wb, sheet = sheet_name, cols = 1, widths = "auto")  # Folder name
+  openxlsx::setColWidths(wb, sheet = sheet_name, cols = 2, widths = "auto")  # Total size
+
+  # Draw vertical lines
+  openxlsx::addStyle(wb, sheet = sheet_name, cols = 1: ncol(df), rows = 2:(nrow(df)+1), gridExpand = TRUE, stack = TRUE,
+                     style = openxlsx::createStyle(border = "LeftRight", borderColour = "#B40036"))
+
+  # Draw line on bottom
+  last_row <- nrow(df) + 1
+  bottomStyle <- openxlsx::createStyle(border = "bottom", borderStyle = "thin", borderColour = "#B40036")
+  openxlsx::addStyle(wb, sheet = sheet_name, style = bottomStyle,
+                     rows = last_row, cols = 1:ncol(df),
+                     gridExpand = TRUE, stack = TRUE)
+}
+
+.add_sheet_for_size <- function(wb, sheet_name, dir, df, show_max, subtitle) {
+
+  ## Make sheet
+  openxlsx::addWorksheet(wb, sheet_name)
+
+  ## Prepare data
+  df$Last_Modified <- as.POSIXct(df$Last_Modified)
+  df_full <- df
+  df_sel <- df[1:show_max, ]
+
+  ## Write data
+
+  # Header and meta info
+  openxlsx::writeData(wb, sheet_name, x = "Selection based on file size", startRow = 1, startCol = 1)
+  openxlsx::writeData(wb, sheet_name, x = subtitle, startRow = 2, startCol = 1)
+
+  openxlsx::writeData(wb, sheet_name, x = dir, startRow = 4, startCol = 1)
+  openxlsx::writeData(wb, sheet_name, x = " - Number of files in directory:", startRow = 5, startCol = 1)
+  openxlsx::writeData(wb, sheet_name, x = " - Total size of files in directory (Bytes):", startRow = 6, startCol = 1)
+  openxlsx::writeData(wb, sheet_name, x = " - Number of files to show in this report:", startRow = 7, startCol = 1)
+  openxlsx::writeData(wb, sheet_name, x = " - Total size of files in this report (Bytes):", startRow = 8, startCol = 1)
+  openxlsx::writeData(wb, sheet_name, x = " - Date of this report:", startRow = 9, startCol = 1)
+
+  openxlsx::writeData(wb, sheet_name, x = nrow(df_full), startRow = 5, startCol = 2)
+  openxlsx::writeData(wb, sheet_name, x = sum(df_full$Size_Bytes, na.rm = T), startRow = 6, startCol = 2)
+  openxlsx::writeData(wb, sheet_name, x = nrow(df_sel), startRow = 7, startCol = 2)
+  openxlsx::writeData(wb, sheet_name, x = Sys.time(), startRow = 8, startCol = 2)  # date (...)
+  openxlsx::writeData(wb, sheet_name, x = sum(df_sel$Size_Bytes, na.rm = T), startRow = 8, startCol = 2)
+
+  openxlsx::addStyle(wb, sheet_name, style = dateStyle, rows = 9, cols = 2, gridExpand = TRUE)
+
+  # Table with meta info
+  openxlsx::writeData(wb, sheet_name, df_sel, startRow = 11, startCol = 1, headerStyle = headerStyle)
+
+
+  ## Define Styles
+  titleStyle <- openxlsx::createStyle(fontSize = 16,  fontColour = "#B40036", textDecoration = "Bold", halign = "LEFT")
+  subtitleStyle <- openxlsx::createStyle(fontSize = 12, halign = "LEFT", textDecoration = "Bold")
+  headerStyle <- openxlsx::createStyle(
+    fontSize = 12, fontColour = "#FFFFFF", fgFill = "#B40036", textDecoration = "Bold",
+    halign = "left", valign = "CENTER", border = "TopBottom", borderColour = "#B40036")
+  dateStyle <- openxlsx::createStyle(numFmt = "yyyy-mm-dd hh:mm:ss")
+  commastyle <- openxlsx::createStyle(numFmt = "COMMA")
+
+  ## Apply styles to the data
+
+  # Header, meta info
+  openxlsx::addStyle(wb, sheet = sheet_name, cols = 1, rows = 1, style = titleStyle, stack = TRUE)
+  openxlsx::addStyle(wb, sheet = sheet_name, cols = 1, rows = 2:9, style = subtitleStyle, stack = TRUE)
+  openxlsx::addStyle(wb, sheet_name, style = commastyle, rows = c(5, 6, 8), cols = 2, gridExpand = TRUE)
+
+  # Set column widths and styles
+  openxlsx::setColWidths(wb, sheet = sheet_name, cols = 1, widths = "auto")  # File name
+  openxlsx::setColWidths(wb, sheet = sheet_name, cols = 2, widths = 20)      # Size
+  openxlsx::setColWidths(wb, sheet = sheet_name, cols = 3, widths = 20)      # Last modified
+
+  # Format column 'Size_Bytes': comma-separated thousands
+  openxlsx::addStyle(wb, sheet = sheet_name, style = commastyle,
+                     cols = 2, rows = 11:(nrow(df) + 11), gridExpand = TRUE, stack = TRUE)
+
+  # Format column 'Last_Modified' as date/time
+  openxlsx::addStyle(wb, sheet = sheet_name, style = dateStyle,
+                     cols = 3, rows = 11:(nrow(df) + 11), gridExpand = TRUE, stack = TRUE)
+
+  # Remove default lines between rows
+  openxlsx::addStyle(wb, sheet = sheet_name, cols = 1: ncol(df), rows = 2:nrow(df), gridExpand = TRUE, stack = TRUE,
+                     style = openxlsx::createStyle(border = "TopBottom", borderColour = 'lightgrey'))
+
+  # Draw vertical lines
+  openxlsx::addStyle(wb, sheet = sheet_name, cols = 1: ncol(df), rows = 11:(nrow(df_sel)+11), gridExpand = TRUE, stack = TRUE,
+                     style = openxlsx::createStyle(border = "LeftRight", borderColour = "#B40036"))
+
+  # Draw line on bottom
+  last_row <- nrow(df_sel) + 11
+  bottomStyle <- openxlsx::createStyle(border = "bottom", borderStyle = "thin", borderColour = "#B40036")
+  openxlsx::addStyle(wb, sheet = sheet_name, style = bottomStyle,
+                     rows = last_row, cols = 1:ncol(df),
+                     gridExpand = TRUE, stack = TRUE)
+}
+
+.add_sheet_for_date <- function(wb, sheet_name, dir, df, show_max, subtitle) {
+
+  ## Make sheet
+  openxlsx::addWorksheet(wb, sheet_name)
+
+  ## Prepare data
+  df$Last_Modified <- as.POSIXct(df$Last_Modified)
+  df_full <- df
+  df_sel <- df[1:show_max, ]
+
+  ## Write data
+
+  # Header and meta info
+  openxlsx::writeData(wb, sheet_name, x = "Selection based on date (last modified)", startRow = 1, startCol = 1)
+  openxlsx::writeData(wb, sheet_name, x = subtitle, startRow = 2, startCol = 1)
+
+  openxlsx::writeData(wb, sheet_name, x = dir, startRow = 4, startCol = 1)
+  openxlsx::writeData(wb, sheet_name, x = " - Number of files in directory:", startRow = 5, startCol = 1)
+  openxlsx::writeData(wb, sheet_name, x = " - Newest file in directory:", startRow = 6, startCol = 1)
+  openxlsx::writeData(wb, sheet_name, x = " - Oldest file in directory:", startRow = 7, startCol = 1)
+  openxlsx::writeData(wb, sheet_name, x = " - Number of files in this report (Bytes):", startRow = 8, startCol = 1)
+  openxlsx::writeData(wb, sheet_name, x = " - Date of this report:", startRow = 9, startCol = 1)
+
+  openxlsx::writeData(wb, sheet_name, x = min(df_full$Last_Modified, na.rm = T), startRow = 6, startCol = 2)     # row 7 is date (...)
+  openxlsx::writeData(wb, sheet_name, x = max(df_full$Last_Modified, na.rm = T), startRow = 5, startCol = 2)     # row 6 is date (...)
+  openxlsx::writeData(wb, sheet_name, x = Sys.time(), startRow = 8, startCol = 2)                                # row 9 is date (...)
+  openxlsx::writeData(wb, sheet_name, x = nrow(df_full), startRow = 5, startCol = 2)
+  openxlsx::writeData(wb, sheet_name, x = nrow(df_sel), startRow = 8, startCol = 2)
+
+  # Table with meta info
+  openxlsx::writeData(wb, sheet_name, df_sel, startRow = 11, startCol = 1, headerStyle = headerStyle)
+
+
+  ## Define Styles
+
+  titleStyle <- openxlsx::createStyle(fontSize = 16,  fontColour = "#B40036", textDecoration = "Bold", halign = "LEFT")
+  subtitleStyle <- openxlsx::createStyle(fontSize = 12, halign = "LEFT", textDecoration = "Bold")
+  headerStyle <- openxlsx::createStyle(
+    fontSize = 12, fontColour = "#FFFFFF", fgFill = "#B40036", textDecoration = "Bold",
+    halign = "left", valign = "CENTER", border = "TopBottom", borderColour = "#B40036")
+  dateStyle <- openxlsx::createStyle(numFmt = "yyyy-mm-dd hh:mm:ss")
+  commastyle <- openxlsx::createStyle(numFmt = "COMMA")
+
+  ## Apply styles to the data
+
+  # Header, meta info
+  openxlsx::addStyle(wb, sheet = sheet_name, cols = 1, rows = 1, style = titleStyle, stack = TRUE)
+  openxlsx::addStyle(wb, sheet = sheet_name, cols = 1, rows = 2:9, style = subtitleStyle, stack = TRUE)
+  openxlsx::addStyle(wb, sheet_name, style = dateStyle, rows = c(6, 7, 9), cols = 2, gridExpand = TRUE)
+  openxlsx::addStyle(wb, sheet_name, style = commastyle, rows = c(5, 8), cols = 2, gridExpand = TRUE)
+
+  # Set column widths and styles
+  openxlsx::setColWidths(wb, sheet = sheet_name, cols = 1, widths = "auto")  # File name
+  openxlsx::setColWidths(wb, sheet = sheet_name, cols = 2, widths = 20)      # Size
+  openxlsx::setColWidths(wb, sheet = sheet_name, cols = 3, widths = 20)      # Last modified
+
+  # Format column 'Last_Modified' as date/time
+  openxlsx::addStyle(wb, sheet = sheet_name, style = dateStyle,
+                     cols = 3, rows = 11:(nrow(df) + 11), gridExpand = TRUE, stack = TRUE)
+
+  # Format column 'Size_Bytes': comma-separated thousands
+  openxlsx::addStyle(wb, sheet = sheet_name, style = commastyle,
+                     cols = 2, rows = 11:(nrow(df) + 11), gridExpand = TRUE, stack = TRUE)
+
+  # Remove default lines between rows
+  openxlsx::addStyle(wb, sheet = sheet_name, cols = 1: ncol(df), rows = 2:nrow(df), gridExpand = TRUE, stack = TRUE,
+                     style = openxlsx::createStyle(border = "TopBottom", borderColour = 'lightgrey'))
+
+  # Draw vertical lines
+  openxlsx::addStyle(wb, sheet = sheet_name, cols = 1: ncol(df), rows = 11:(nrow(df_sel)+11), gridExpand = TRUE, stack = TRUE,
+                     style = openxlsx::createStyle(border = "LeftRight", borderColour = "#B40036"))
+
+  # Draw line on bottom
+  last_row <- nrow(df_sel) + 11
+  bottomStyle <- openxlsx::createStyle(border = "bottom", borderStyle = "thin", borderColour = "#B40036")
+  openxlsx::addStyle(wb, sheet = sheet_name, style = bottomStyle,
+                     rows = last_row, cols = 1:ncol(df),
+                     gridExpand = TRUE, stack = TRUE)
+}
+
+

f635b5bffc618f97c818823ab933b4f3c05eaa6f
buchjani <janine.buchholz@hu-berlin.de>
2026-05-08 16:52:11 +0200

    added fix to handle "()" in xlsx sheet names #43

--- R/utils_convert_xlsx_to_csv.R
+++ R/utils_convert_xlsx_to_csv.R
@@ -23,25 +23,29 @@
   }
 
   # get sheet names
-  sheet_names <- openxlsx::getSheetNames(tolower(xlsx_path))
+  # sheet_names <- openxlsx::getSheetNames(tolower(xlsx_path))  # fails when sheet names contain "()"
+  wb <- suppressWarnings(openxlsx2::wb_load(xlsx_path))
+  sheet_names <- openxlsx2::wb_get_sheet_names(wb)
 
   # prepare output directory
   if (!dir.exists(save_to)) dir.create(save_to, recursive = TRUE)
 
   # build output paths: one CSV per sheet
+  out_names <- fs::path_sanitize(sheet_names)
   out_files <- file.path(save_to,
-                         paste0(tools::file_path_sans_ext(basename(xlsx_path)), "_", sheet_names, ".csv"))
+                         paste0(tools::file_path_sans_ext(basename(xlsx_path)), "_", out_names, ".csv"))
 
   # iterate over sheets
   for (i in seq_along(sheet_names)) {
-    df <- suppressWarnings(openxlsx::read.xlsx(tolower(xlsx_path), sheet = sheet_names[i]))
+    # df <- suppressWarnings(openxlsx::read.xlsx(tolower(xlsx_path), sheet = sheet_names[i])) # fails when sheet names contain "()"
+    df <- suppressWarnings(openxlsx2::wb_to_df(xlsx_path, sheet = sheet_names[i]))
     .write_csv_utf8_bom(df, out_files[i], sep = sep, dec = dec)
   }
 
   invisible(out_files)
 }
 
-# # check
+## check
 # .convert_xlsx_to_csv(xlsx_path = "Q:/FDZ/Alle/99_MitarbeiterInnen/JB/eatArchive/20251110_Demo/TVD/Diverse Dateiformate/Excel-Datei mit Großbuchstaben.XLSX",
 #                     save_to = "Q:/FDZ/Alle/99_MitarbeiterInnen/JB/eatArchive/20251110_Demo/TVD_AIP",
 #                     csv = "csv2")

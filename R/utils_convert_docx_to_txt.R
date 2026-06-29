4b8d6dea8e4df01dd68cb8882d45f8e0d2f29569
buchjani <janine.buchholz@hu-berlin.de>
2026-06-29 12:16:54 +0200

    removed pandoc-dependency

--- R/utils_convert_docx_to_txt.R
+++ R/utils_convert_docx_to_txt.R
@@ -9,36 +9,17 @@
 .convert_docx_to_txt <- function(docx_path,
                                  save_to = dirname(docx_path)) {
 
-  is_docx <- grepl("\\.docx$", docx_path, ignore.case = TRUE)
-
   out <- file.path(
     save_to,
     paste0(tools::file_path_sans_ext(basename(docx_path)), ".txt")
   )
-
-  if (is_docx) {
-    if (nzchar(Sys.which("pandoc"))) {
-      system2(
-        "pandoc",
-        c("-s", docx_path, "-t", "plain", "-o", out)
-      )
-    } else if (requireNamespace("readtext", quietly = TRUE)) {
-      txt <- readtext::readtext(docx_path)$text
-      writeLines(txt, out, useBytes = TRUE)
-    } else {
-      stop(
-        "Converting .docx-files requires either 'pandoc' or the R package 'readtext'."
-      )
-    }
-  } else {
+  if (grepl("\\.docx$", docx_path, ignore.case = TRUE)) {   # docx-files
+    txt <- readtext::readtext(docx_path)$text
+  } else {                                                  # doc-files
     txt <- antiword::antiword(docx_path)
-    txt <- iconv(
-      txt,
-      from = "WINDOWS-1252",
-      to = "UTF-8"
-    )
-    writeLines(txt, out)
+    txt <- iconv(txt, from = "WINDOWS-1252", to = "UTF-8")
   }
+  writeLines(txt, out)
   out
 }
 
